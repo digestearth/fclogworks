491e4743cb9b7990a039cafe9f7492329de87e89
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2024-08-27 19:15:05 -0700

    Made photo carousel slightly better looking

--- src/components/emblacarousel.jsx
+++ src/components/emblacarousel.jsx
@@ -12,47 +12,77 @@ const CarouselContainer = styled.div`
     position:fixed;
     top: 0;
     left: 0;
+    height: 100%;
     width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
-    min-height: 100%;
-    background-color: #000000d7;
+    
+    background-color: #000000e8;
 
     .embla {
-        height: 100%;
-        width: 90%;
         overflow: hidden;
-        --slide-spacing: 5rem;
+       height: 100%;
+        /* --slide-spacing: 2rem; */
     }
     .embla__container {
         touch-action: pan-y pinch-zoom;
         display: flex;
-        margin-left: calc(var(--slide-spacing) * -1);
+        /* margin-left: calc(var(--slide-spacing) * -1); */
+        height: 100%;
+        width: 100%;
     }
     .embla__slide {
-        flex: 0 0 auto;
+        flex: 0 0 100%;
+        height: 100%;
+        /* padding-left: var(--slide-spacing); */
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
 
-        max-height: 75vh;
+    img {
+        max-height: 80%;
+    }
 
-        min-width: 0;
-        max-width: 90%;
-        padding-left: var(--slide-spacing);
-        img {
-            max-width: 100%;
+    .caption {
+        height: 10%;
+        width: 60%;
+        /* background-color: black; */
+        color: white;
+        bottom: 0;
 
-            max-height: 75vh;
-            min-height: 100%;
+        p {
+            margin: 10px;
         }
     }
+
+
 
     button {
         position: fixed;
         top: 5vh;
         right: 5vh;
     }
-    
 `
+
+function PortfolioEntry(props){
+    const {photo} = props
+    return (
+        <div className="embla__slide">
+            <img src={photo.src} alt={photo.alt}/>
+            <div className="caption"><p>{photo.desc}</p></div>
+            {/* <div className = "portfolio_item">
+                <div className = "photo">
+                    <img src={photo.src} alt={photo.alt}/>
+                </div>
+                <div className = "caption"><p>{photo.desc}</p></div>
+            </div> */}
+            
+        </div>
+    )
+}
 
 export function Carousel(props) {
     const {carouselActive, setCarouselActive, photos} = props
@@ -70,7 +100,7 @@ export function Carousel(props) {
             <div className="embla" ref={emblaRef}>
                 <div className="embla__container">
                     {Object.keys(photos).map(key=>{
-                        return <div key={key} className="embla__slide"><img src={photos[key].src} alt={photos[key].alt}></img></div>
+                        return <PortfolioEntry key={key} photo={photos[key]}/>
                     })}
                 </div>
             </div>
