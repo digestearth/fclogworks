b7f2f9a9587d9abee4d29dd7f91b82dff5d914ac
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2024-08-29 16:15:30 -0700

    Added images to gallery skeleton page

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -1,26 +1,61 @@
+import { useOutletContext } from 'react-router-dom';
 import styled from '@emotion/styled'
+
+function galleryButtonClicked(index, setCarouselActive) {
+    setCarouselActive(true);
+    // setHamburgerActive(false);
+    // setPhotos(photoArray);
+    console.log("Gallery button clicked: ", index);
+}
 
 const GalleryContainer = styled.div`
     margin-left: auto;
     margin-right: auto;
-    width: 80%;
+    width: 100%;
 
     display: flex;
-    background-color: #75b975;   
+    justify-content: center;
+    background-color: #75b975;
+    flex-wrap: wrap;
 `
 
-const Gallerycard = styled.div`
-    width: 200px;
-    height: 200px;
+const GalleryCard = styled.div`
+    width: 300px;
+    height: 300px;
     background-color: #7a8ff7;
     margin: 10px;
+    overflow: hidden;
+    border-radius: 10px;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
 `
 
-export function Gallery(props) {
-
+function GalleryPhoto(props) {
+    const {photo, setCarouselActive, index} = props
     return (
+        <GalleryCard onClick={() => galleryButtonClicked(index, setCarouselActive)}>
+            <img src={photo.thumb} alt={photo.alt}/>
+        </GalleryCard>
+    )
+}
+
+export function Gallery(props) {
+    const {photos} = props;
+    const {setCarouselActive} = useOutletContext();
+    return (
+        <>
+        Title
+        Description
         <GalleryContainer>
-            <h1>Gallery</h1>
+            {Object.keys(photos).map((key, index)=>{
+                return <GalleryPhoto key={key} index={index} photo={photos[key]} setCarouselActive={setCarouselActive}/>
+            })}
         </GalleryContainer>
+        </>
+        
     )
 }
