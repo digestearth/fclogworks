4260e71c75420fb7e54c0ad3da87e54abeb4556d
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2024-11-02 15:24:21 -0700

    feat: changed saturation of site color

--- src/components/colors.jsx
+++ src/components/colors.jsx
@@ -4,7 +4,8 @@
 
 export const color_bg = "#ffffff";
 export const color_text = "#373734";
-export const color_element = "#9b7b65";
+//export const color_element = "#9b7b65";
+export const color_element = "#9c7155";
 export const color_subelement = "#f9f8f6";
 export const color_element_inset = "#8e715d";
 export const color_element_accent = "#70594f";
